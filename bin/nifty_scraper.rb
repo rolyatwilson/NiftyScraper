315e237acfbfe76e0303ed1a488cfb75e4d4f92f
Taylor Wilson <twilson@instructure.com>
2017-11-20 22:33:54 -0700

    update cli

--- bin/nifty_scraper.rb
+++ bin/nifty_scraper.rb
@@ -5,14 +5,19 @@ require_relative '../lib/nifty_scraper'
 module NiftyScraper
   module CLI
     class Main < Thor
-      desc 'wiki_states', 'Nifty Scraper'
+      desc 'wiki_states', 'Parses states and basic data from Wikipedia and outputs json to disk'
       def wiki_states
-        data = WikiStates.parse
-        WikiStates.write_json_file(data)
+        NiftyScraper.write_json_file(WikiStates.parse, WikiStates.default_path)
         puts 'So Nifty! \o/'
       end
 
-      default_command :wiki_states
+      desc 'start', 'Merges custom json data with scraped json data.'
+      def start
+        NiftyScraper.write_json_file(Merger.squash, Merger.default_path)
+        puts 'So Nifty! \o/'
+      end
+
+      default_command :start
     end
   end
 end
